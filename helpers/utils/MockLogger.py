79539dc476ac86f46b247c5b9336983a2bb1c267
Kyle Weiher <kyle.weiher@wits.ac.za>
2020-05-27 09:52:45 +0200

    Add MockLogger.py for mocking logging functions.

---
+++ helpers/utils/MockLogger.py
@@ -0,0 +1,22 @@
+class MockLogger:
+    """
+    A class to mock a logger.
+    """
+    def __init__(self):
+        """
+        A class to mock a logger.
+        """
+    def debug(self, message: str):
+        pass
+
+    def info(self, message: str):
+        pass
+
+    def warning(self, message: str):
+        pass
+
+    def error(self, message: str):
+        pass
+
+    def critical(self, message: str):
+        pass
