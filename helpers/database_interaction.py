c7e9fc43087d6d23a3a455a2a6296016107c5045
Kyle Weiher <kyle.weiher@wits.ac.za>
2020-05-28 16:24:50 +0200

    Add logging to all functions, update to use check_existence_of_table, execute_select_query_on_db and execute_action_query_on_db functions internally.

--- helpers/database_interaction.py
+++ helpers/database_interaction.py
@@ -41,53 +41,83 @@ def check_existence_of_table(
 
 
 def get_db_table_column_names(
-    table_name: str, engine: __sq__.engine
+    table_name: str, engine: __sq__.engine, logger: __Logger__ = None
 ) -> __Optional__[list]:
     """
-    Get column names of table on database.
+    Get column names of table on database. Checks for existence of table first.
 
-    :param engine: (sqlalchemy.engine) DB engine used for DB connection.
+    :param engine: (sqlalchemy.engine): DB engine used for DB connection.
     :param table_name: (str): Name of table to perform operation on.
     :return: (Optional[list]): List of column names, None if table does not exist.
+    :param logger: (logging.Logger): Logger to use for logging.
     """
-    query: str = helpers.utils.generate_column_names_of_db_table_query(table_name)
-    with helpers.ConnectionManager(engine) as conn:
-        try:
-            result = __pd__.read_sql(query, conn)
-            col_names = list(result.columns)
-            return col_names
-        except __sq__.exc.DatabaseError as error:
-            if "table or view does not exist" in str(error):
-                return None
+
+    if logger is None:
+        logger = helpers.utils.MockLogger()
+
+    if helpers.check_existence_of_table(table_name, engine):
+        query: str = helpers.utils.generate_column_names_of_db_table_query(table_name)
+
+        success_msg: str = "Successfully retrieved column names of '{table_name}'.".format(
+            table_name=table_name
+        )
+        error_msg: str = "Failed to retrieve column names of '{table_name}'.".format(
+            table_name=table_name
+        )
+
+        result: __pd__.DataFrame = helpers.execute_select_query_on_db(
+            query, success_msg, error_msg, engine, logger
+        )
+        col_names: list = list(result.columns)
+        logger.debug("Column Names: {column_names}".format(column_names=col_names))
+        return col_names
+    else:
+        return None
 
 
-def get_db_table_row_count(table_name: str, engine: __sq__.engine) -> __Optional__[int]:
+def get_db_table_row_count(
+    table_name: str, engine: __sq__.engine, logger: __Logger__ = None
+) -> __Optional__[int]:
     """
-    Get row count of table on database.
+    Get row count of table on database. Checks for existence of table first.
 
-    :param engine: (sqlalchemy.engine) DB engine used for DB connection.
+    :param logger: (logging.Logger): Logger to use for logging.
+    :param engine: (sqlalchemy.engine): DB engine used for DB connection.
     :param table_name: (str): Name of table to perform operation on.
-    :return: Number of rows, None if table does not exist.
+    :return: (int): Number of rows, None if table does not exist.
     """
+
+    if logger is None:
+        logger = helpers.utils.MockLogger()
+
     query: str = helpers.utils.generate_get_number_of_rows_of_db_table_query(table_name)
-    with helpers.ConnectionManager(engine) as conn:
-        try:
-            result = __pd__.read_sql(query, conn)
-            count: int = result["COUNT(*)"].iloc[0]
-            return count
-        except __sq__.exc.DatabaseError as error:
-            if "table or view does not exist" in str(error):
-                return None
+
+    success_msg: str = "Successfully retrieved row count for table '{table_name}'.".format(
+        table_name=table_name
+    )
+    error_msg: str = "Failed to retrieve row count for table '{table_name}'.".format(
+        table_name=table_name
+    )
+
+    try:
+        result: __pd__.DataFrame = execute_select_query_on_db(
+            query, success_msg, error_msg, engine, logger
+        )
+        count: int = result["COUNT(*)"].iloc[0]
+        return count
+    except helpers.LoggedDatabaseError as error:
+        if "table or view does not exist" in str(error):
+            return None
 
 
 def truncate_table(
     table_name: str, engine: __sq__.engine, logger: __Logger__ = None
 ) -> None:
     """
-    Truncate staging or prod table.
+    Truncate staging or prod table. Checks for existence of table first.
 
     :param logger: (logging.logger): Logger to use for logging
-    :param engine: (sqlalchemy.engine) DB engine used for DB connection.
+    :param engine: (sqlalchemy.engine): DB engine used for DB connection.
     :param table_name: (str): Name of table to perform operation on.
     :return: None
     """
@@ -95,56 +125,47 @@ def truncate_table(
     if logger is None:
         logger = helpers.utils.MockLogger()
 
-    query: str = helpers.utils.generate_trunc_db_table_query(table_name)
+    if helpers.check_existence_of_table(table_name, engine):
+        query: str = helpers.utils.generate_trunc_db_table_query(table_name)
 
-    with helpers.ConnectionManager(engine) as conn:
-        trans = conn.begin()
-        try:
-            conn.execute(query)
-            logger.info(
-                "Truncated table {table_name} on DB.".format(table_name=table_name)
-            )
-        except Exception as e:
-            if "table or view does not exist" in str(e):
-                pass
-            else:
-                raise helpers.LoggedDatabaseError(logger, str(e))
+        success_msg: str = "Successfully truncated table '{table_name}'.".format(
+            table_name=table_name
+        )
+        error_msg: str = "Failed to truncated table '{table_name}'.".format(
+            table_name=table_name
+        )
 
-        trans.commit()
+        helpers.execute_action_query_on_db(
+            query, success_msg, error_msg, engine, logger
+        )
 
 
 def drop_table(
     table_name: str, engine: __sq__.engine, logger: __Logger__ = None
 ) -> None:
     """
-    Drop staging or prod table.
+    Drop table. Checks for existence of table first.
 
     :param logger: (logging.logger): Logger to use for logging.
-    :param engine: (sqlalchemy.engine) DB engine used for DB connection.
+    :param engine: (sqlalchemy.engine): DB engine used for DB connection.
     :param table_name: (str): Name of table to perform operation on.
-    :param staging: (bool): If True, drop staging table, else prod table will be dropped.
     :return: None
     """
 
     if logger is None:
         logger = helpers.utils.MockLogger()
 
-    query: str = helpers.utils.generate_drop_db_table_query(table_name)
+    if helpers.check_existence_of_table(table_name, engine):
+        query: str = helpers.utils.generate_drop_db_table_query(table_name)
 
-    with helpers.ConnectionManager(engine) as conn:
-        trans = conn.begin()
-        try:
-            conn.execute(query)
-            logger.info(
-                "Dropped table {table_name} on DB.".format(table_name=table_name)
-            )
-        except Exception as e:
-            if "table or view does not exist" in str(e):
-                pass
-            else:
-                raise helpers.LoggedDatabaseError(logger, str(e))
+        success_msg: str = "Successfully dropped '{table_name}'".format(
+            table_name=table_name
+        )
+        error_msg: str = "Failed to drop '{table_name}'".format(table_name=table_name)
 
-        trans.commit()
+        helpers.execute_action_query_on_db(
+            query, success_msg, error_msg, engine, logger
+        )
 
 
 def create_table(
@@ -158,20 +179,20 @@ def create_table(
     """
     Create table. If table already exists, check to see that the column names
     match for compatibility.
+
     :param allow_nulls: (bool): Allow nulls in table.
     :param use_date_created: (bool): Include a 'DATE_CREATED' column with current date and time.
     :param logger: (logging.logger): Logger to use for logging.
-    :param engine: (sqlalchemy.engine) DB engine used for DB connection.
+    :param engine: (sqlalchemy.engine): DB engine used for DB connection.
     :param table_name: (str): Name of table to perform operation on.
     :param data_results: (pd.DataFrame): Data to use for generating column names and data types.
-    :param staging: (bool): If True, create staging table, else prod table will be created.
     :return: None
     """
 
     if logger is None:
         logger = helpers.utils.MockLogger()
 
-    # Check if table already exists
+    # Check if table already exists and get it's column names.
     db_col_names: __Optional__[list] = get_db_table_column_names(table_name, engine)
     # If the table does not exist
     if not db_col_names:
@@ -180,25 +201,15 @@ def create_table(
             data_results, table_name, allow_nulls, use_date_created,
         )
 
-        with helpers.ConnectionManager(engine) as conn:
-            trans = conn.begin()
+        success_msg: str = "Created table '{table_name}'.".format(table_name=table_name)
+        error_msg: str = "Failed to create '{table_name}'.".format(
+            table_name=table_name
+        )
 
-            try:
-                conn.execute(create_query)
-                logger.info(
-                    "Created table {table_name} on DB.".format(table_name=table_name)
-                )
-            except Exception as e:
-                if "name is already used by an existing object" in str(e):
-                    logger.info(
-                        "Table {table_name} already exists on DB.".format(
-                            table_name=table_name
-                        )
-                    )
-                else:
-                    raise helpers.LoggedDatabaseError(logger, str(e))
+        helpers.execute_action_query_on_db(
+            create_query, success_msg, error_msg, engine, logger
+        )
 
-            trans.commit()
     else:
         # Formatting
         db_col_names: list = [x.upper() for x in db_col_names]
@@ -220,8 +231,14 @@ def create_table(
             diff: set = (union - new_data_col_names_set).union(union - db_col_names_set)
             raise helpers.LoggedDataError(
                 logger,
-                "data columns do not match database columns: {diff} for table {table_name}".format(
+                "Local data columns do not match DB columns: {diff} for table '{table_name}'".format(
                     diff=diff, table_name=table_name
+                ),
+            )
+        else:
+            logger.debug(
+                "Column names on DB match those of local data for table '{table_name}'.".format(
+                    table_name=table_name
                 ),
             )
 
@@ -269,14 +286,16 @@ def upload_data_to_table(
             ],
             table_name,
         )
-        success_msg: str = "Uploaded rows: {a} - {b}".format(
+        success_msg: str = "Uploaded rows: {a} - {b} to '{table_name}'".format(
             a=str(iterator_index * upload_partition_size),
             b=str((iterator_index + 1) * upload_partition_size - 1),
+            table_name=table_name,
         )
 
-        error_msg: str = "Failed to upload rows: {a} - {b}".format(
+        error_msg: str = "Failed to upload rows: {a} - {b} to '{table_name}'".format(
             a=str(iterator_index * upload_partition_size),
             b=str((iterator_index + 1) * upload_partition_size - 1),
+            table_name=table_name,
         )
 
         helpers.execute_action_query_on_db(
@@ -284,22 +303,21 @@ def upload_data_to_table(
         )
         iterator_index += 1
 
-    print(
-        "Uploading rows: {a} - {b}".format(
-            a=str(iterator_index * upload_partition_size), b=str(data_num_records),
-        )
-    )
     # Final non divisible rows
     query: str = helpers.utils.generate_insert_query(
         table_data[iterator_index * upload_partition_size :], table_name,
     )
 
-    success_msg: str = "Uploaded rows: {a} - {b}".format(
-        a=str(iterator_index * upload_partition_size), b=str(data_num_records),
+    success_msg: str = "Uploaded rows: {a} - {b} to '{table_name}'".format(
+        a=str(iterator_index * upload_partition_size),
+        b=str(data_num_records),
+        table_name=table_name,
     )
 
-    error_msg: str = "Failed to upload rows: {a} - {b}".format(
-        a=str(iterator_index * upload_partition_size), b=str(data_num_records),
+    error_msg: str = "Failed to upload rows: {a} - {b} to '{table_name}'".format(
+        a=str(iterator_index * upload_partition_size),
+        b=str(data_num_records),
+        table_name=table_name,
     )
 
     helpers.execute_action_query_on_db(query, success_msg, error_msg, engine, logger)
@@ -319,7 +337,7 @@ def update_column_by_value(
 
     :param column_name: (str): Name of column to update.
     :param logger: (logging.logger): Logger to use for logging.
-    :param engine: (sqlalchemy.engine) DB engine used for DB connection.
+    :param engine: (sqlalchemy.engine): DB engine used for DB connection.
     :param table_name: (str): Name of table to perform operation on.
     :param old_value: (int): Value to select rows by.
     :param new_value: (int): Value to replace old value.
@@ -331,13 +349,13 @@ def update_column_by_value(
     query: str = helpers.utils.generate_update_column_by_value_query(
         table_name, column_name, old_value, new_value,
     )
-    success_msg: str = "Updated rows in {table_name} table: {column_name}={old_value} -> {column_name}={new_value}.".format(
+    success_msg: str = "Updated rows in '{table_name}' table: '{column_name}'='{old_value}' -> '{column_name}'='{new_value}'.".format(
         table_name=table_name,
         old_value=old_value,
         new_value=new_value,
         column_name=column_name,
     )
-    error_msg: str = "Failed to update rows in {table_name} table: {column_name}={old_value} -> {column_name}={new_value}.".format(
+    error_msg: str = "Failed to update rows in '{table_name}' table: '{column_name}'='{old_value}' -> '{column_name}'='{new_value}'.".format(
         table_name=table_name,
         old_value=old_value,
         new_value=new_value,
@@ -354,6 +372,16 @@ def execute_select_query_on_db(
     engine: __sq__.engine,
     logger: __Logger__ = None,
 ) -> __pd__.DataFrame:
+    """
+    Execute a returning select query.
+
+    :param query: (str): Query to be executed.
+    :param success_msg: (str): Debug message for successful execution.
+    :param error_msg: (str): Error message for failed execution.
+    :param engine: (sqlalchemy.engine): DB engine used for DB connection.
+    :param logger: (logging.logger): Logger to use for logging.
+    :return: (pd.Dataframe): Date returned from DB.
+    """
 
     if logger is None:
         logger = helpers.utils.MockLogger()
@@ -375,6 +403,16 @@ def execute_action_query_on_db(
     engine: __sq__.engine,
     logger: __Logger__ = None,
 ) -> None:
+    """
+    Execute a non-returning, commit required query.
+
+    :param query: (str): Query to be executed.
+    :param success_msg: (str): Debug message for successful execution.
+    :param error_msg: (str): Error message for failed execution.
+    :param engine: (sqlalchemy.engine): DB engine used for DB connection.
+    :param logger: (logging.logger): Logger to use for logging.
+    :return: None
+    """
 
     if logger is None:
         logger = helpers.utils.MockLogger()
