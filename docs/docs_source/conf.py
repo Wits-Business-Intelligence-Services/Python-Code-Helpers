c3c87b8c6fbaa65de33bed5a56fe7675790f7454
Kyle Weiher <kyle.weiher@wits.ac.za>
2020-06-17 14:04:52 +0200

    Update docs conf.py.

--- docs/docs_source/conf.py
+++ docs/docs_source/conf.py
@@ -24,13 +24,7 @@ copyright = "2020, Kyle Weiher, Dhruv Bhugwan"
 author = "Kyle Weiher, Dhruv Bhugwan"
 
 # The full version, including alpha/beta/rc tags
-release = "1.0.0"
-
-# import os
-# import sys
-
-# sys.path.insert(0, os.path.abspath("../"))
-# sys.path.insert(0, os.path.abspath("../BISCodeHelpers/"))
+release = "1.0.1"
 
 import sphinx_rtd_theme
 
@@ -64,4 +58,5 @@ html_theme = "sphinx_rtd_theme"
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ["_static"]
+# html_static_path = ["_static"]
+html_static_path = []
