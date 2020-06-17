3184b92ea2ebc8b31643d230233ba4763696a651
Kyle Weiher <kyle.weiher@wits.ac.za>
2020-06-17 14:04:05 +0200

    Remove old commented code from __init__.py

--- bis_code_helpers/__init__.py
+++ bis_code_helpers/__init__.py
@@ -7,28 +7,6 @@ from .run_external_command import *
 # from bis_code_helpers.library_backend import *
 from .library_backend import *
 
-# from bis_code_helpers import (
-#     ConnectionManager,
-#     create_engine,
-#     LoggedValueError,
-#     LoggedDataError,
-#     LoggedDatabaseError,
-#     LoggedSubprocessError,
-#     setup_logging,
-#     get_db_table_column_names,
-#     get_db_table_row_count,
-#     truncate_table,
-#     drop_table,
-#     create_table,
-#     upload_data_to_table,
-#     update_column_by_value,
-#     execute_select_query_on_db,
-#     execute_action_query_on_db,
-#     check_existence_of_table,
-#     current_db_compatible_time,
-# )
-# from bis_code_helpers.library_backend import set_mock_logging_level, LoggingLevels
-
 # Select only those objects that you want imported
 __all__ = [
     "ConnectionManager",
