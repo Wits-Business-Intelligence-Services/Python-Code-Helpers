644f0227ad655cb4ff1dd8cba8e9c6c0d9895182
Kyle Weiher <kyle.weiher@wits.ac.za>
2022-03-30 13:19:03 +0200

    Limit string length when uploading data, set to 3990 for leeway. Version 1.2.15.

--- setup.py
+++ setup.py
@@ -18,7 +18,7 @@ URL = 'https://github.com/me/myproject'
 EMAIL = 'kyle.weiher@wits.ac.za'
 AUTHOR = 'Wits BIS'
 REQUIRES_PYTHON = '>=3.7.0'
-VERSION = '1.2.14'
+VERSION = '1.2.15'
 
 # What packages are required for this module to be executed?
 REQUIRED = [
