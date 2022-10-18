370489dbf159611028b8370a2c18f8c655a7c52b
Kyle Weiher <Kyle.Weiher@wits.ac.za>
2022-10-18 13:58:16 +0200

    Add logging to run_external_command.py. Version 1.3.0.

--- bis_code_helpers/run_external_command.py
+++ bis_code_helpers/run_external_command.py
@@ -1,7 +1,11 @@
 import subprocess as __subprocess__
+import logging as __logging__
+from typing import Optional as __Optional__
+
+import bis_code_helpers
 
 
-def run_external_command(command_arg_list: list) -> str:
+def run_external_command(command_arg_list: list, logger: __Optional__[__logging__.Logger] = None) -> str:
     """
     Run an external command, wait for it to finish, check return code, and return
     combined stdout and stderr as as string.
@@ -9,14 +13,20 @@ def run_external_command(command_arg_list: list) -> str:
     arg list is in the format ['executable', 'arg0', 'arg1', ...].
 
     :param command_arg_list: (list): List of args.
+    :param logger: (logging.Logger): Logger to use for failure logging.
     :return: (str): Combined stdout and stderr.
     """
     p: __subprocess__.run = __subprocess__.run(
         command_arg_list, stdout=__subprocess__.PIPE, stderr=__subprocess__.STDOUT
     )
-    # Check that the return code is a success.
-    p.check_returncode()
-
     stdout: str = p.stdout.decode()
-
+    # Check that the return code is a success.
+    try:
+        p.check_returncode()
+    except Exception as e:
+        if logger:
+            raise bis_code_helpers.LoggedSubprocessError(logger, f"{str(e)}\n\nThe output of the subprocess is: {stdout}")
+        else:
+            print(stdout)
+            raise e
     return stdout
